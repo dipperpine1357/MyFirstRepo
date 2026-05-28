287a5bce6ba89ac89128a192a6378df09f56f93e
Dipper Pine <dipper.pine1357@gmail.com>
2026-05-28 09:49:26 +0300

    add tichnut q 1 & 2

---
+++ tichnut/python/ex_2.py
@@ -0,0 +1,25 @@
+# Name: ex_2.py
+# Author: dipper
+# Date: 27.5.2026
+# Description: the code takes the mouse to random places on the screen every 10 seconds
+# imports
+import win32api
+import random, time
+
+# this function generates random x & y places on the screen
+def random_xy():
+    x_cordinate = random.randint(0, 1920)
+    y_cordinate = random.randint(0, 1080)
+    return x_cordinate, y_cordinate
+
+def main():
+    while 1:
+        x_cordinate, y_cordinate = random_xy()
+        win32api.mouse_event(1, x_cordinate, y_cordinate, 0)
+        print ("x_cordinate-> ", x_cordinate,"\n", "y_cordinate-> ", y_cordinate)
+        print ("to stop press cntrl + c")
+        time.sleep(10)
+
+
+if __name__=="__main__":
+    main()
