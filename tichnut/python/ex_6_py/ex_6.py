32a335d9fb21a0ca07a2876688925dbe4859d0bf
Dipper Pine <dipper.pine1357@gmail.com>
2026-05-31 20:14:53 +0300

    add tichnut ex 6 python

--- tichnut/python/ex_6_py/ex_6.py
+++ tichnut/python/ex_6_py/ex_6.py
@@ -7,7 +7,8 @@ import requests
 
 #CONSTANTS
 DOMAIN_PATH = r"https://www.ynet.co.il"
-FILE_PATH = r"C:\dipper\my_first_repo\tichnut\python\ex_6_py\common_files_names_ex6_py.txt"
+# FILE_PATH = r"C:\dipper\my_first_repo\tichnut\python\ex_6_py\common_files_names_ex6_py.txt"
+GOOD_STATUSE_CODE = 200
 
 # this func takes all the names
 # this function returns all the seccessfull url names
@@ -15,36 +16,33 @@ def requesting (names, base_url):
     good_names = []
     current_url = ""
     for name in names:
-        # print (name)
         current_url = base_url + "/" + name
         response = requests.get(current_url)
 
         # is the statuse code 200
-        if (response.status_code == 200):
+        if (response.status_code == GOOD_STATUSE_CODE):
             good_names.append(name)
-            # print ("hh")
 
             #is it a dir
             if (response.content != None):
 
                 # doing recursion for directories
                 good_names.append(requesting(names, current_url))
-                # print (good_names)
     return good_names
 
 def main():
     url = DOMAIN_PATH
     names = []
     seccess = []
-    with open(FILE_PATH, "r") as file:
+    user_path = input("enter path \n")
+    with open(user_path, "r") as file:
         for line in file:
             line = line.strip()
             names.append(line)
             url = DOMAIN_PATH + "/" + line
-            # print (names)
             seccess = requesting(names, DOMAIN_PATH)
-    print (f"""all the fitting names (if they are in [] then """
-           f"""they are from the dir of the name before them)\n{seccess}""")
+    print (f"""\nall the fitting names (if they are in [] then """
+           f"""they are from the dir of the name before them):\n{seccess}""")
             
 
 
